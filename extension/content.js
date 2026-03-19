62dacd58c47f80217f44d076702ee0c0ecd235f6
Bricio Blancas <blancasbricio977@gmail.com>
2026-03-18 21:58:00 -0700

    Update connected backend url

--- extension/content.js
+++ extension/content.js
@@ -52,7 +52,7 @@ function showAnalyzeButton(selectedText, x, y) {
     showResultPopup("Analyzing...", clickX, clickY);
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("http://localhost:8080/predict", {
             method: "POST",
             headers: {
           "Content-Type": "application/json"
